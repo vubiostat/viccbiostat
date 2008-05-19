539cf7cfa96320ba34e180d8740b2ae696b9f98a
Will Gray <w.gray@vanderbilt.edu>
2008-05-19 14:37:42 +0000

    Added na.rm option to normalize.
    Use an in-line function for apply instead of itself to save a few steps.

--- R/normalize.R
+++ R/normalize.R
@@ -1,13 +1,13 @@
-normalize <- function(x, margin = c(0,1,2), rename = TRUE)
+normalize <- function(x, margin = c(0,1,2), rename = TRUE, na.rm = TRUE)
 {
     margin <- match.arg(margin)
     if (margin == 0 || is.null(dim(x)))
     {
-        result <- (x - mean(x))/sd(x)
+        result <- (x - mean(x, na.rm=na.rm))/sd(x, na.rm=na.rm)
     }
     else
     {
-        result <- apply(x, margin, normalize)
+        result <- apply(x, margin, function(x) (x - mean(x, na.rm=na.rm))/sd(x, na.rm=na.rm))
         if (margin == 1)
             result <- t(result)
         if (!identical(dim(result), dim(x)))
