a2e6aa6f0057f81ee9ca047c81de96eb856c4843
Will Gray <w.gray@vanderbilt.edu>
2008-05-19 10:52:32 -0500

    Call the C method correctly.

--- R/fdrAdjustment.R
+++ R/fdrAdjustment.R
@@ -5,7 +5,7 @@ fdrAdjustment <- function(data, ranks=NULL, method=c("step.up", "step.down"))
     if (missing(ranks))
         ranks <- rank(data)
     q <- (data * length(data) / ranks)[order(data)]
-    q <- .C(fdrAdjustment,
+    q <- .C("fdrAdjust",
         q = as.double(q),
         as.integer(length(q)),
         as.integer(stepUp))$q[ranks]
