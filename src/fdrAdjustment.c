52bfc1554654ee042692bf1a55924c17071219a0
Will Gray <w.gray@vanderbilt.edu>
2008-06-03 16:08:09 -0500

    Formatting.

--- src/fdrAdjustment.c
+++ src/fdrAdjustment.c
@@ -1,22 +1,16 @@
 #include <math.h>
 
-void fdrAdjust(double *q, int *n, int *stepUp)
-{
+void fdrAdjust(double *q, int *n, int *stepUp) {
     int i;
     double p = 1;
 
-    if (*stepUp)
-    {
-        for (i = *n - 1; i >= 0; i--)
-        {
+    if (*stepUp) {
+        for (i = *n - 1; i >= 0; i--) {
             if (q[i] < p) p = q[i];
             q[i] = p;
         }
-    }
-    else
-    {
-        for (i = 0; i < *n; i++)
-        {
+    } else {
+        for (i = 0; i < *n; i++) {
             if (q[i] > p) p = q[i];
             q[i] = p;
         }
