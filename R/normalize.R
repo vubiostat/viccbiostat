fb786af08bd4749126421ae074d235e643253856
Will Gray <w.gray@vanderbilt.edu>
2008-06-17 10:43:29 -0500

    Cannot use numerics with match.arg, so do some conversions.

--- R/normalize.R
+++ R/normalize.R
@@ -1,5 +1,5 @@
-normalize <- function(x, margin = c(0,1,2), rename = TRUE, na.rm = TRUE) {
-    margin <- match.arg(margin)
+normalize <- function(x, margin = 0, rename = TRUE, na.rm = TRUE) {
+    margin <- as.numeric(match.arg(as.character(margin), c('0', '1', '2')))
     if (margin == 0 || is.null(dim(x)))
         result <- (x - mean(x, na.rm=na.rm))/sd(x, na.rm=na.rm)
     else {
