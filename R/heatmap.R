fa44a22f5ccec2b18f6d9e332ca371c4d822a4da
Will Gray <w.gray@vanderbilt.edu>
2009-04-22 16:38:37 -0500

    Cleaning up some thing in heapmap wrapper.

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,11 +1,11 @@
-htmp <- function(dat=NULL, info=NULL, smp=NULL, grp=NULL, tle="heatmap", labx="gene", laby="patient", autosave=FALSE) {
-    if (is.null(dat) || is.null(info))
+htmp <- function(data=NULL, info=NULL, smp=NULL, grp=NULL, main="heatmap", xlab="gene", ylab="patient", col=cm.colors(256), filename=NULL) {
+    if (is.null(data) || is.null(info))
         stop("Please provid both of data set and information set.")
-    if (dim(dat)[1] != dim(info)[1])
-        stop("Data set and information set should have same numer of patients.")
+    if (nrow(data) != nrow(info))
+        stop("Data set and information set should have same number of samples.")
 
-    dat <- as.matrix(dat)
-    l <- dim(dat)[2]
+    data <- as.matrix(data)
+    l <- ncol(data)
     cc <- rainbow(l, start=0, end=max(1,l-1)/l)
     rc <- palette()[as.integer(factor(info[,grp]))+1]
 
@@ -17,24 +17,23 @@ htmp <- function(dat=NULL, info=NULL, smp=NULL, grp=NULL, tle="heatmap", labx="g
     if (length(cc) >= 50 )
         labcol = NA
     else
-        labcol = colnames(dat)
+        labcol = colnames(data)
 
-    if (autosave)
-        png(filename = "heatmap.png", width = 480, height = 480,
-            units = "px", pointsize = 12, bg = "white", res = NA,
-            restoreConsole = TRUE)
+    if (!is.null(filename))
+        png(filename=filename, width=480, height=480, units="px", pointsize=12, bg="white", res=NA, restoreConsole=TRUE)
 
-    heatmap(dat,
-            col = cm.colors(256),
+    heatmap(data,
             scale="column",
-            RowSideColors = rc,
-            ColSideColors = cc,
+            RowSideColors=rc,
+            ColSideColors=cc,
             margins=c(5,10),
-            labRow = labrow,
-            labCol = labcol,
-            xlab = labx,
-            ylab= laby,
-            main = tle)
+            labRow=labrow,
+            labCol=labcol,
+            main=main,
+            xlab=xlab,
+            ylab=ylab,
+            col=col)
 
-    if (autosave) dev.off()
+    if (!is.null(filename))
+        dev.off()
 }
