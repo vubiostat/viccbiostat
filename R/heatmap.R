6f323092150c79c3349a5ade89b19dd7483566b5
Joan Zhang <xueqiong.zhang@vanderbilt.edu>
2009-04-22 16:25:08 -0500

    Next version of heatmap wrapper.

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,11 +1,40 @@
-heat<-function(data){
-    x <- as.matrix(data)
-    d <- ncol(x[,c(2:8)])
-    cc <- rainbow(d, start=0, end=0.3)
-    hv <- heatmap(x[, c(2:8)], col=cm.colors(256), scale="column",
-        RowSideColors=palette()[ x[, "group"]+2 ],
-        ColSideColors=cc,
-        margins=c(5,10),
-        xlab="winners ", ylab="Patients",
-        main="heatmap")
+htmp <- function(dat=NULL, info=NULL, smp=NULL, grp=NULL, tle="heatmap", labx="gene", laby="patient", autosave=FALSE) {
+    if (is.null(dat) || is.null(info))
+        stop("Please provid both of data set and information set.")
+    if (dim(dat)[1] != dim(info)[1])
+        stop("Data set and information set should have same numer of patients.")
+
+    dat <- as.matrix(dat)
+    l <- dim(dat)[2]
+    cc <- rainbow(l, start=0, end=max(1,l-1)/l)
+    rc <- palette()[as.integer(factor(info[,grp]))+1]
+
+    if (length(rc) >=50)
+        labrow = NA
+    else
+        labrow = info$smp
+
+    if (length(cc) >= 50 )
+        labcol = NA
+    else
+        labcol = colnames(dat)
+
+    if (autosave)
+        png(filename = "heatmap.png", width = 480, height = 480,
+            units = "px", pointsize = 12, bg = "white", res = NA,
+            restoreConsole = TRUE)
+
+    heatmap(dat,
+            col = cm.colors(256),
+            scale="column",
+            RowSideColors = rc,
+            ColSideColors = cc,
+            margins=c(5,10),
+            labRow = labrow,
+            labCol = labcol,
+            xlab = labx,
+            ylab= laby,
+            main = tle)
+
+    if (autosave) dev.off()
 }
