bdf3b2572627983c92e638c9225f9ab25fdcc202
Will Gray <w.gray@vanderbilt.edu>
2009-04-24 12:44:03 -0500

    Getting our heatmap code usable.

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,39 +1,43 @@
-htmp <- function(data=NULL, info=NULL, smp=NULL, grp=NULL, main="heatmap", xlab="gene", ylab="patient", col=cm.colors(256), filename=NULL) {
-    if (is.null(data) || is.null(info))
-        stop("Please provid both of data set and information set.")
-    if (nrow(data) != nrow(info))
-        stop("Data set and information set should have same number of samples.")
+htmp <- function(data=NULL, group=NULL, main="heatmap", xlab="feature", ylab="sample", col=RColorBrewer::brewer.pal(12,"Set3"), filename=NULL, png.args=NULL, heat.colors=NULL) {
 
-    data <- as.matrix(data)
-    l <- ncol(data)
-    cc <- rainbow(l, start=0, end=max(1,l-1)/l)
-    rc <- palette()[as.integer(factor(info[,grp]))+1]
+    require('stats') || stop("package 'stats' is missing")
+    if (is.null(heat.colors) || !is.null(filename))
+        require('grDevices') || stop("package 'grDevices' is missing")
 
-    if (length(rc) >=50)
-        labrow = NA
+    if (is.data.frame(data))
+        data <- as.matrix(data[sapply(data, mode) == "numeric"])
+    # get colors by group index
+    if (is.factor(group))
+        rc <- col[as.integer(group)]
+    # assume colors are already defined
     else
-        labrow = info$smp
+        rc <- group
 
-    if (length(cc) >= 50 )
-        labcol = NA
-    else
-        labcol = colnames(data)
+    if (is.null(heat.colors)) {
+        i <- seq(0, 1, length.out=256)
+        heat.colors <- c(hsv(h=0.9794372, v=i))
+        if (min(data) < 0)
+            heat.colors <- c(heat.colors, rev(hsv(h=0.5839329, v=i)))
+    }
+
+    labrow <- rownames(data)
+    if (is.null(labrow) || length(labrow) >= 50)
+        labrow <- NA
+
+    labcol <- colnames(data)
+    if (is.null(labcol) || length(labcol) >= 50)
+        labcol <- NA
 
     if (!is.null(filename))
-        png(filename=filename, width=480, height=480, units="px", pointsize=12, bg="white", res=NA, restoreConsole=TRUE)
+        do.call('png', c(filename=filename, png.args))
+        #png(filename=filename, width=480, height=480, units="px", pointsize=12, bg="white", res=NA, restoreConsole=TRUE)
 
-    heatmap(data,
-            scale="column",
-            RowSideColors=rc,
-            ColSideColors=cc,
-            margins=c(5,10),
-            labRow=labrow,
-            labCol=labcol,
-            main=main,
-            xlab=xlab,
-            ylab=ylab,
-            col=col)
+    result <- heatmap(data, scale="column", margins=c(5,10),
+            RowSideColors=rc, labRow=labrow, labCol=labcol,
+            main=main, xlab=xlab, ylab=ylab, col=heat.colors)
 
     if (!is.null(filename))
         dev.off()
+
+    invisible(result)
 }
