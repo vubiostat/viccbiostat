def6da2f64db8b4b7968ad4942ec63c04e4d3d2d
Will Gray <graywh@gmail.com>
2009-05-07 13:53:39 -0500

    Too many uses of as.factor.

--- R/accuracy.R
+++ R/accuracy.R
@@ -1,9 +1,9 @@
 accuracy <- function(actual, prediction) {
     if (length(actual) != length(prediction))
         stop("All arguments must have the same length.")
-    lvls <- union(levels(as.factor(actual, exclude=NULL)), levels(as.factor(prediction, exclude=NULL)))
-    actual <- as.factor(actual, lvls, exclude=NULL)
-    prediction <- as.factor(prediction, lvls, exclude=NULL)
+    lvls <- union(levels(factor(actual, exclude=NULL)), levels(factor(prediction, exclude=NULL)))
+    actual <- factor(actual, lvls, exclude=NULL)
+    prediction <- factor(prediction, lvls, exclude=NULL)
     tmp <- table(actual, prediction, exclude=NULL)
     result <- diag(tmp) / rowSums(tmp)
     if (any(is.na(lvls))) {
